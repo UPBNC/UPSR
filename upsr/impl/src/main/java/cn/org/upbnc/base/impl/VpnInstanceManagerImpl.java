386ac2f460a1e9e534d6391c5d8f4542f17b85e2
cyyang <cyyang@bnc.org.cn>
2018-11-20 15:18:35 +0800

    拓扑实例管理实现

--- upsr/impl/src/main/java/cn/org/upbnc/base/impl/VpnInstanceManagerImpl.java
+++ upsr/impl/src/main/java/cn/org/upbnc/base/impl/VpnInstanceManagerImpl.java
@@ -7,16 +7,18 @@
  */
 package cn.org.upbnc.base.impl;
 
-import cn.org.upbnc.entity.VPNInstance;
+import cn.org.upbnc.entity.*;
 import cn.org.upbnc.base.VpnInstanceManager;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Iterator;
 
 public class VpnInstanceManagerImpl  implements VpnInstanceManager {
     private static VpnInstanceManager instance = null;
-   // private List<VPNInstance>  vpnInstanceList ;
+    private List<VPNInstance>  vpnInstanceList ;
 
     @Override
     public String toString() {
@@ -25,8 +27,7 @@ public class VpnInstanceManagerImpl  implements VpnInstanceManager {
 
     private VpnInstanceManagerImpl()
     {
-      //  this.vpnInstanceList = new LinkedList<VPNInstance>();
-
+       this.vpnInstanceList = new LinkedList<VPNInstance>();
     }
     public static VpnInstanceManager getInstance()
     {
@@ -36,4 +37,130 @@ public class VpnInstanceManagerImpl  implements VpnInstanceManager {
         }
         return instance;
     }
+    public boolean addVpnInstance(VPNInstance vpnInstance)
+    {
+        if(null == vpnInstance)
+        {
+            return false;
+        }
+        vpnInstanceList.add(vpnInstance);
+        return true;
+    }
+    public boolean delVpnInstance(Integer id)
+    {
+        Iterator<VPNInstance> iter =  vpnInstanceList.iterator();
+        while(iter.hasNext())
+        {
+            VPNInstance vpnInstance = iter.next();
+            if(id == vpnInstance.getId().intValue())
+            {
+                iter.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean delVpnInstance(String vpnName)
+    {
+        if(null == vpnName)
+            return false;
+        Iterator<VPNInstance> iter = vpnInstanceList.iterator();
+        while(iter.hasNext())
+        {
+            VPNInstance vpnInstance = iter.next();
+            if(true == vpnInstance.getVpnName().equals(vpnName) )
+            {
+                iter.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+    public VPNInstance  getVpnIstance(String vpnName)
+    {
+        if(null == vpnName)
+            return null;
+        Iterator<VPNInstance> iter = vpnInstanceList.iterator();
+        while(iter.hasNext())
+        {
+            if(true == iter.next().getVpnName().equals(vpnName))
+            {
+                return iter.next();
+            }
+        }
+        return null;
+    }
+    public VPNInstance getVpnInstance(String routerId)
+    {
+        if(null == routerId)
+            return null;
+        Iterator<VPNInstance> iter = vpnInstanceList.iterator();
+        while(iter.hasNext())
+        {
+            if(true == iter.next().getRd().equals(routerId))
+            {
+                return iter.next();
+            }
+        }
+        return null;
+    }
+    public VPNInstance updateVpnInstance(String vpnName,
+                                         Device device,
+                                         String businessRegion,
+                                         String rd,
+                                         String importRT,
+                                         String exportRT,
+                                         Integer peerAS,
+                                         Address peerIP,
+                                         Integer routeSelectDelay,
+                                         Integer importDirectRouteEnable,
+                                         List<DeviceInterface> deviceInterfaceList,
+                                         List<NetworkSeg> networkSegList)
+    {
+        if(null == vpnName)
+            return null;
+        VPNInstance  vpnInstance = getVpnInstance(vpnName);
+        if(null != vpnInstance)
+        {
+            vpnInstance.setDevice(device);
+            vpnInstance.setBusinessRegion(businessRegion);
+            vpnInstance.setRd(rd);
+            vpnInstance.setImportRT(importRT);
+            vpnInstance.setExportRT(exportRT);
+            vpnInstance.setPeerAS(peerAS);
+            vpnInstance.setPeerIP(peerIP);
+            vpnInstance.setRouteSelectDelay(routeSelectDelay);
+            vpnInstance.setImportDirectRouteEnable(importDirectRouteEnable);
+            vpnInstance.setDeviceInterfaceList(deviceInterfaceList);
+            vpnInstance.setNetworkSegList(networkSegList);
+
+        }
+        else
+        {
+            Integer id = 0 ;
+            vpnInstance = new VPNInstance(id, device, deviceInterfaceList, vpnName, businessRegion,rd,
+                    importRT, exportRT, peerAS, peerIP, routeSelectDelay, importDirectRouteEnable, networkSegList);
+           vpnInstanceList.add(vpnInstance);
+        }
+        return vpnInstance;
+    }
+
+    public VPNInstance  updateVpnInstance(VPNInstance vpnInstance)
+    {
+        if(null == vpnInstance)
+            return null;
+        VPNInstance  findVpnInstance = getVpnInstance(vpnInstance.getVpnName());
+        if(null != findVpnInstance)
+        {
+            vpnInstance.setId(findVpnInstance.getId());
+            findVpnInstance = vpnInstance;
+            return findVpnInstance;
+        }
+        else
+        {
+            vpnInstanceList.add(vpnInstance);
+        }
+        return vpnInstance;
+    }
+
 }
