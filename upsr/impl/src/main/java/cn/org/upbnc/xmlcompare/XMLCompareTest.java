571110f7c50cf03d0ccd2533551ab803656e0f64
libingkui <13938562465@163.com>
2019-06-12 14:47:02 +0800

    log add

--- upsr/impl/src/main/java/cn/org/upbnc/xmlcompare/XMLCompareTest.java
+++ upsr/impl/src/main/java/cn/org/upbnc/xmlcompare/XMLCompareTest.java
@@ -1,37 +1,39 @@
 package cn.org.upbnc.xmlcompare;
 
 import cn.org.upbnc.util.netconf.SSrTeTunnel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class XMLCompareTest {
+    private static final Logger LOG = LoggerFactory.getLogger(XMLCompareTest.class);
     public static void test() {
         String xml1 = Util.candidate();
         String xml2 = Util.modify();
         ActionEntity actionEntity = XmlUtils.compare(xml1, xml2);
-        System.out.println(actionEntity.getPath());
         if (ActionTypeEnum.add.name().equals(actionEntity.getAction().name())) {
-            System.out.println("add");
+            LOG.info("add");
             List<SSrTeTunnel> sSrTeTunnels = GetXml.getSrTeTunnelFromXml(xml1, AttributeParse.parse(actionEntity.getPath()));
             if (sSrTeTunnels.size() != 0) {
-                System.out.println(sSrTeTunnels.get(0).toString());
+                LOG.info(sSrTeTunnels.get(0).toString());
             }
         } else if (ActionTypeEnum.delete.name().equals(actionEntity.getAction().name())) {
-            System.out.println("delete");
+            LOG.info("delete");
             List<SSrTeTunnel> sSrTeTunnels = GetXml.getSrTeTunnelFromXml(xml2, AttributeParse.parse(actionEntity.getPath()));
             if (sSrTeTunnels.size() != 0) {
-                System.out.println(sSrTeTunnels.get(0).toString());
+                LOG.info(sSrTeTunnels.get(0).toString());
             }
         } else if (ActionTypeEnum.modify.name().equals(actionEntity.getAction().name())) {
             List<SSrTeTunnel> sSrTeTunnels = GetXml.getSrTeTunnelFromXml(xml1, AttributeParse.parse(actionEntity.getPath()));
-            System.out.println("modify");
+            LOG.info("modify");
             if (sSrTeTunnels.size() != 0) {
-                System.out.println(sSrTeTunnels.get(0).getTunnelName());
+                LOG.info(sSrTeTunnels.get(0).getTunnelName());
                 List<ModifyEntity> modifyEntities = actionEntity.getModifyEntities();
                 for (ModifyEntity modifyEntity : modifyEntities) {
-                    System.out.println("lable :" + modifyEntity.getLabel());
-                    System.out.println("modifyEntity.getOdlValue() :" + modifyEntity.getOdlValue());
-                    System.out.println("modifyEntity.getNewValue() :" + modifyEntity.getNewValue());
+                    LOG.info("lable :" + modifyEntity.getLabel());
+                    LOG.info("modifyEntity.getOdlValue() :" + modifyEntity.getOdlValue());
+                    LOG.info("modifyEntity.getNewValue() :" + modifyEntity.getNewValue());
                 }
             }
         }
